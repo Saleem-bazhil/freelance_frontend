7ba89690992e4cc290b3b88f2f2bc1b5748aa4ca
saleem <ab588538@gmail.com>
2026-02-27 17:31:00 +0530

    update

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 // e.g., 'http://192.168.1.xxx:8000'
 // Use your machine's IP address if testing on a physical device over Wi-Fi.
 // If testing on an Android Emulator, change this to: 'http://10.0.2.2:8000'
-const API_URL = 'http://127.0.0.1:8000';
+// const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'https://freelance-backend-pwh5.onrender.com';
+
 
 const api = axios.create({
     baseURL: API_URL,
